9f560e193ca5f5b20b1f5a01e2c199ae31d28182
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-13 13:07:40 +0300

    adding custom register logic
    This will be refactored soon

--- WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/auth/register/register.component.ts
+++ WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/auth/register/register.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
+  constructor(private userService: UserService, private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  // TODO: Add register logic.
+
+  registerHandler(): void {
+    this.userService.login();
+    this.router.navigate(['/home']);
   }
-
 }
