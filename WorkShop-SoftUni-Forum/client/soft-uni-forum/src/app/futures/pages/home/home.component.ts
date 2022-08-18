61429d8d93ca476c89011bbf02847ef3e02bf8bc
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-18 15:50:44 +0300

    edit home login/register btns functionality

--- WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/futures/pages/home/home.component.ts
+++ WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/futures/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from 'src/app/auth.service';
 import { UserService } from 'src/app/services/user.service';
 
 @Component({
@@ -7,14 +9,9 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  isLoggedIn: Observable<boolean> = this.authService.isLoggedIn$;
 
-  get isLoggedIn(): boolean {
-    return this.userServise.loggedIn;
-  }
-
-  constructor(private userServise: UserService) {
-    
-  }
+  constructor(private authService: AuthService) {}
 
   ngOnInit(): void {}
 }
