b95506f6bbf2f784da852699a406d6dd5549ebe4
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-19 13:11:45 +0300

    Update auth.guard.ts

--- WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/core/guards/auth.guard.ts
+++ WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/core/guards/auth.guard.ts
@@ -1,25 +1,23 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, Router, UrlTree } from '@angular/router';
+import { map, Observable, take } from 'rxjs';
 import { AuthService } from 'src/app/auth.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AuthGuard implements CanActivate {
-  
-  isLoggedIn!: boolean;
-
   constructor(private authService: AuthService, private router: Router) {}
 
-  canActivate(): boolean | UrlTree {
-    this.authService.isLoggedIn$.subscribe((isLoggedIn) => {
-      this.isLoggedIn = isLoggedIn;
-    });
-
-    if (this.isLoggedIn) {
-      return true;
-    } else {
-      return this.router.createUrlTree(['/user/login']);
-    }
+  canActivate(): Observable<boolean | UrlTree> {
+    return this.authService.isLoggedIn$.pipe(
+      take(1),
+      map((isLoggedIn) => {
+        if (isLoggedIn) {
+          return true;
+        }
+        return this.router.createUrlTree(['/user/login']);
+      })
+    );
   }
 }
