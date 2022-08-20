d5e49a248918f3f42eb09ec3ae346e4b7142dbbd
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-20 15:38:08 +0300

    add can subscribe btn logic

--- WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/futures/theme/themes-list/themes-list.component.ts
+++ WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/futures/theme/themes-list/themes-list.component.ts
@@ -1,7 +1,13 @@
-import { Component, Input, OnChanges } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  Component,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
+import { map, Observable, take } from 'rxjs';
 import { AuthService } from 'src/app/auth.service';
-import { ITheme } from 'src/app/interfaces';
+import { ITheme, IUser } from 'src/app/interfaces';
 
 @Component({
   selector: 'app-themes-list',
@@ -12,14 +18,23 @@ export class ThemesListComponent implements OnChanges {
   @Input() theme!: ITheme;
 
   isLoggedIn$: Observable<boolean> = this.authService.isLoggedIn$;
-  canSubscribe: boolean = false;
+  canSubscribe!: boolean;
+  isOwner!: boolean;
+  currentUser!: IUser | undefined;
 
   constructor(private authService: AuthService) {}
 
   ngOnChanges(): void {
-    // TODO: ADD BETER FUNCTIONALITY!
-    this.canSubscribe = !this.theme.subscribers.includes(
-      '5fa64b162183ce1728ff371d'
+    this.authService.currentUser$.pipe(
+      take(1),
+      map((user) => {
+        this.currentUser = user;
+      })
     );
+
+    this.isOwner = this.theme.userId._id == this.currentUser?._id;
+    this.canSubscribe = this.currentUser
+      ? this.theme.subscribers.includes(this.currentUser._id)
+      : false;
   }
 }
