103390f925a96024de1d221946ab45ecd8039b78
Pavlin Evtimov <pavlinevtimov@yahoo.com>
2022-08-21 21:34:35 +0300

    Update theme-content.component.ts

--- WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/futures/theme/theme-content/theme-content.component.ts
+++ WorkShop-SoftUni-Forum/client/soft-uni-forum/src/app/futures/theme/theme-content/theme-content.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { combineLatest, mergeMap, Observable, Subscription, tap } from 'rxjs';
+import { combineLatest, mergeMap, Observable, Subscription } from 'rxjs';
 import { AuthService } from 'src/app/auth.service';
 import { IPost, IUser } from 'src/app/interfaces';
 import { ITheme } from 'src/app/interfaces';
@@ -20,7 +20,7 @@ export class ThemeContentComponent implements OnInit, OnDestroy {
   theme!: ITheme;
   comments!: IPost[];
 
-  subscribtion$!: Subscription;
+  subscribtion$: Subscription = new Subscription();
   isLoggedIn$: Observable<boolean> = this.authService.isLoggedIn$;
   currentUser: IUser | undefined;
 
@@ -33,18 +33,20 @@ export class ThemeContentComponent implements OnInit, OnDestroy {
   ) {}
 
   ngOnInit(): void {
-    this.subscribtion$ = combineLatest([
-      this.activatedRoute.params.pipe(
-        mergeMap((params) => {
-          const themeId = params['themeId'];
-          return this.themeService.getThemeById$(themeId);
-        })
-      ),
-      this.authService.currentUser$,
-    ]).subscribe(([theme, user]) => {
-      (this.theme = theme), (this.comments = theme.posts);
-      this.currentUser = user;
-    });
+    this.subscribtion$.add(
+      combineLatest([
+        this.activatedRoute.params.pipe(
+          mergeMap((params) => {
+            const themeId = params['themeId'];
+            return this.themeService.getThemeById$(themeId);
+          })
+        ),
+        this.authService.currentUser$,
+      ]).subscribe(([theme, user]) => {
+        (this.theme = theme), (this.comments = theme.posts);
+        this.currentUser = user;
+      })
+    );
   }
 
   canLike(likes: string[]): boolean {
@@ -56,14 +58,14 @@ export class ThemeContentComponent implements OnInit, OnDestroy {
   }
 
   like(comment: IPost): void {
-    this.subscribtion$ = this.postService
-      .like$(comment._id)
-      .subscribe(({ message }) => {
+    this.subscribtion$.add(
+      this.postService.like$(comment._id).subscribe(({ message }) => {
         this.messageBus.notifyForMessage({
           text: message,
           type: MessageType.Success,
         });
-      });
+      })
+    );
 
     if (this.currentUser) {
       comment.likes.push(this.currentUser._id);
@@ -71,14 +73,14 @@ export class ThemeContentComponent implements OnInit, OnDestroy {
   }
 
   dislike(comment: IPost): void {
-    this.subscribtion$ = this.postService
-      .dislike$(comment._id)
-      .subscribe(({ message }) => {
+    this.subscribtion$.add(
+      this.postService.dislike$(comment._id).subscribe(({ message }) => {
         this.messageBus.notifyForMessage({
           text: message,
           type: MessageType.Success,
         });
-      });
+      })
+    );
 
     if (this.currentUser) {
       comment.likes = comment.likes.filter(
@@ -89,6 +91,5 @@ export class ThemeContentComponent implements OnInit, OnDestroy {
 
   ngOnDestroy(): void {
     this.subscribtion$.unsubscribe();
-    console.log('asd');
   }
 }
